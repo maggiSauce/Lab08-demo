92b8ff5c5f1637f8e7a7bdbe404c673aa116693b
Ben B <bbui@ualberta.ca>
2026-03-03 17:49:52 -0700

    broken count

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -20,4 +20,12 @@ public class CustomListTest {
         list.deleteCity(calgary);
         assertFalse(list.hasCity(calgary));
     }
+
+    @Test
+    public void testCountCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertEquals(list.countCities(), 1);
+    }
 }
